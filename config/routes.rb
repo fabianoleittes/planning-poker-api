551680c138a182b4dcbbf6bcf0036d8f852253fa
Fabiano Leite <fabianoleittes@gmail.com>
2016-08-18 22:34:36 -0300

    Add routes for resources stories.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   scope defaults: { format: :json }, path: "/" do
     namespace :v1 do
-      resources :backlogs, only: %i[index show create update destroy]
+      resources :backlogs, only: %i[index show create update destroy] do
+        resources :stories, only: %i[create update destroy]
+      end
     end
   end
 end
